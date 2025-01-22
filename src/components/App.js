163c1989b22ecdfaff641fb09092eaf651c5865c
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-01-22 17:54:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [name, setName] = useState("");
+  function handleInput(e) {
+    setName(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input type="text" onInput={handleInput} />
+      {name != "" ? <p>Hello {name}!</p> : null}
     </div>
   )
 }
